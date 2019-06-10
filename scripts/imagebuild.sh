9d97669001fdaca5e6737233b2e59a99c710e4f5
iuri.maftei <iuri.maftei@cloudreach.com>
2019-06-10 17:37:09 +0100

    Updte file content

--- scripts/imagebuild.sh
+++ scripts/imagebuild.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-set -v
 
 export AWS_DEFAULT_PROFILE=cr-labs-skill-up
-ARTIFACT=`packer build -var 'profile=cr-labs-skill-up' -machine-readable packer-demo.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+#ARTIFACT=`packer build -machine-readable packer-demo.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+AMI_ID=$(packer build -machine-readable packer-demo.json 2>&1 | awk '{FS=":"}{print $NF}' | grep ami\-|tail -1)
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
-aws s3 cp amivar.tf s3://terraform-state-c29df1kjsadfkjb/amivar.tf --profile cr-labs-skill-up
+aws s3 cp amivar.tf s3://terraform-state-c29df1kjsadfkjb/amivar.tf
